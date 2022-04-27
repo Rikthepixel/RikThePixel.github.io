4d2ab4da540ddb074c372b7d1224fe19f8f72b6d
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-04-27 22:23:17 +0200

    [Change] Improved SEO

--- src/views/Front/index.jsx
+++ src/views/Front/index.jsx
@@ -10,21 +10,19 @@ const Front = () => {
 
   return (
     <>
-      <PageHeader className="min-h-[auto]  sm:min-h-screen mt-12 mb-auto sm:my-0">
+      <PageHeader className="min-h-[unset] sm:min-h-screen mt-12 mb-auto sm:my-0">
         <div>
           <p className="text-[5vmin]">Hello!</p>
           <p>I'm Rik!</p>
         </div>
         <div className="hero-action move-down font-extrabold text-[10vmin] hidden sm:block" aria-label="Go down">↓</div>
       </PageHeader>
-      <div className="w-2/3 grid grid-col-1 sm:grid-cols-2 gap-4 mb-auto sm:mb-0">
-        <div className="flex justify-center sm:justify-end">
-          <img className="aspect-square object-cover rounded-md w-full sm:w-4/5 lg:w-1/2 max-h-[50vh] md:max-h-[80vh]" src={AboutMePhoto} />
+      <div className="w-2/3 gap-4 mb-auto flex justify-center flex-wrap children:w-full children:flex children:justify-center children:items-center sm:mb-20 sm:flex-nowrap sm:children:w-auto md:gap-8">
+        <div className="">
+          <img className="w-full h-fit aspect-square object-cover rounded-full max-w-[15rem]" src={AboutMePhoto} />
         </div>
-        <div className="flex justify-center items-center sm:justify-start">
-          <div className="text-center">
-            Hallow, have a look at my projects!
-          </div>
+        <div className="text-center p-4">
+          Hello, have a look at my projects!
         </div>
       </div>
     </>
