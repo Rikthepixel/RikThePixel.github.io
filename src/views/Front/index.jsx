3cad13c159ec67dd929c11cbd33ab8b2d9eed398
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-04-28 13:57:01 +0200

    [Addition] Hero-short text and hero-long text

--- src/views/Front/index.jsx
+++ src/views/Front/index.jsx
@@ -19,9 +19,13 @@ const Front = () => {
       </PageHeader>
       <div className="w-2/3 gap-4 mb-auto flex justify-center flex-wrap children:w-full children:flex children:justify-center children:items-center sm:mb-20 sm:flex-nowrap sm:children:w-auto md:gap-8">
         <div className="">
-          <img className="w-full h-fit aspect-square object-cover rounded-full max-w-[15rem]" src={AboutMePhoto} />
+          <img className="w-full h-fit aspect-square object-cover rounded-full max-w-[10rem] sm:max-w-[15rem]" src={AboutMePhoto} />
         </div>
-        <div className="text-center p-4">
+        <div className="hero-short">
+          Hello, this website is still work in progress! <br />
+          Stay tuned!
+        </div>
+        <div className="hero-long">
           Hello, this website is still work in progress! <br />
           Stay tuned!
         </div>
