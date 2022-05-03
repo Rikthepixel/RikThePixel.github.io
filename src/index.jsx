d2bbf6556047c303214a329a5ee13d634fec0c50
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-05-03 20:34:08 +0200

    [Addition] Add pre-rendering for better SEO

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import "./index.scss";
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import Routes from "./routes";
 
+const getRoot = () => {
+    const rootElement = document.getElementById('react-root');
+    return rootElement.hasChildNodes() ? hydrateRoot(rootElement) : createRoot(rootElement);
+};
 
-const root = createRoot(document.getElementById('react-root'));
+const root = getRoot();
+
 root.render(
     <React.StrictMode>
         <HashRouter>
