54639a8bf4faf9ebe00dc953b63848465f56f8ff
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-05-03 13:12:32 +0200

    [Change] Improve front page accessibility

--- src/views/Front/index.jsx
+++ src/views/Front/index.jsx
@@ -17,25 +17,39 @@ const Front = () => {
         </div>
         <div className="hero-action move-down font-extrabold text-[10vmin] hidden sm:block" aria-label="Go down">↓</div>
       </PageHeader>
-      <div className="w-2/3 gap-4 mb-auto flex justify-center flex-wrap children:w-full children:flex children:justify-center children:items-center pb-32 sm:pb-24 sm:flex-nowrap sm:children:w-auto md:gap-8">
+      <section
+        aria-label="About me"
+        className="w-2/3 gap-4 mb-auto flex justify-center flex-wrap children:w-full children:flex children:justify-center children:items-center pb-32 sm:pb-24 sm:flex-nowrap sm:children:w-auto md:gap-8"
+      >
         <div className="">
-          <img className="w-full h-fit aspect-square object-cover rounded-full max-w-[10rem] sm:max-w-[15rem]" src={AboutMePhoto} />
+          <img
+            aria-label="Image"
+            className="w-full h-fit aspect-square object-cover rounded-full max-w-[10rem] sm:max-w-[15rem]"
+            src={AboutMePhoto}
+            alt="A guy"
+          />
         </div>
-        <div className="hero-short">
-          <p>Hello, my name is Rik den Breejen.</p>
-          <p>I'm a Software engineering student at Fontys.</p>
+        <div
+          aria-label="Description"
+          className="hero-short"
+        >
+          <p aria-label="Who I am">Hello, my name is Rik den Breejen.</p>
+          <p aria-label="What I do">I'm a Software engineering student at Fontys.</p>
         </div>
-        <div className="hero-long">
-          <p>
+        <div
+          aria-label="Description"
+          className="hero-long"
+        >
+          <p aria-label="Who I am">
             Hello, my name is Rik den Breejen. I'm a Dutch Software engineering student at Fontys.<br />
             I have a passion for coding! I often spend a lot of my free time programming.
           </p>
-          <p>
+          <p aria-label="What I am currently learning">
             Currently learning: <br />
             <a href="https://vitejs.dev/" target="_blank">ViteJS ⚡</a> and <a href="https://love2d.org/" target="_blank">Love2D</a>
           </p>
         </div>
-      </div>
+      </section>
     </>
   );
 };
