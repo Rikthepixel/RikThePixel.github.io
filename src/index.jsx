e0eef3c2ba2d283952757dbc7a20159b87764fb3
RikThePixel <Rikdenbreejen@hotmail.nl>
2022-05-03 22:41:40 +0200

    [Fix] Hydration error in production

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,9 @@ import { createRoot, hydrateRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import Routes from "./routes";
 
-const getRoot = () => {
-    const rootElement = document.getElementById('react-root');
-    return rootElement.hasChildNodes() ? hydrateRoot(rootElement) : createRoot(rootElement);
-};
-
-const root = getRoot();
-
-root.render(
+createRoot(
+    document.getElementById('react-root')
+).render(
     <React.StrictMode>
         <HashRouter>
             <Routes />
