b6a308a7d454910dc7761dd097ef654c68e5a3ed
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-05-21 23:27:07 +0200

    [Change] Fix size of header text

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageHeader = ({ className = "", children }) => {
     return (
-        <h1 id="page-header" className={`${className} pb-4 sm:pb-4 mt-auto sm:mt-auto sm:min-h-[unset] min-h-[unset] text-[15vmin] flex flex-col items-center justify-center relative text-center`}>
+        <h1 id="page-header" className={`${className} pb-4 sm:pb-4 mt-auto sm:mt-auto sm:min-h-[unset] min-h-[unset] text-[5rem] sm:text-[9rem] flex flex-col items-center justify-center relative text-center`}>
             {children}
         </h1>
     );
