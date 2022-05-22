6195b3211a9169af49e054bb23625c33c6b8d848
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-05-22 23:53:20 +0200

    Improve sitemap output

--- builders/sitemap.js
+++ builders/sitemap.js
@@ -1,12 +1,18 @@
 import Routes from "../src/routes";
 const Generator = require('react-router-sitemap-generator').default;
 
-const generator = new Generator(
-    'https://rikthepixel.github.io/#',
-    Routes(),
-    {
-        lastmod: new Date().toISOString().slice(0, 10),
-        changefreq: 'monthly',
-    }
-);
-generator.save('dist/sitemap.xml');
+try {
+    const generator = new Generator(
+        'https://rikthepixel.github.io/#/',
+        Routes(),
+        {
+            lastmod: new Date().toISOString().slice(0, 10),
+            changefreq: 'monthly',
+            
+        }
+    );
+    generator.save('dist/sitemap.xml');
+    console.log("Sitemap succesfully generated");
+} catch (err) {
+    console.log("Failed to generate sitemap", err);
+}
