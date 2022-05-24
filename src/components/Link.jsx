21295c838e3aff702eabfc0ba70f68add0a223f2
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-05-24 23:31:32 +0200

    Give link more padding

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Link = ({ className = "", href, children = [], ...props }) => (
-    <a {...props} className={`transition-colors duration-200 ${className} w-fit text-white bg-purple-700 hover:bg-purple-500 rounded-md p-1 inline-block`} href={href}>
+    <a {...props} className={`transition-colors duration-200 ${className} w-fit text-white bg-purple-700 hover:bg-purple-500 rounded-md px-[1.5ch] py-[0.5ch] inline-block`} href={href}>
         {children}
     </a>);
 
