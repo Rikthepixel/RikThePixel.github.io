d67ae3465e8f35d1853320754ea70bded7d0c07a
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-07-17 18:19:24 +0200

    Fix project imports

--- src/views/Projects/features/Project/index.jsx
+++ src/views/Projects/features/Project/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ProjectLinks from "./ProjectLinks";
-import ProjectTechnologies from "./ProjectTechnologies";
-import ProjectIcon from "./ProjectIcon";
-import ProjectHeading from "./ProjectHeading";
+import Links from "./Links";
+import Technologies from "./Technologies";
+import Icon from "./Icon";
+import Heading from "./Heading";
 
 const Project = ({
     title, text, icon, iconAlt, startDate, endDate = { custom: "Now" }, links, technologies, lazy
@@ -11,15 +11,15 @@ const Project = ({
     const startDateString = startDate.custom ?? `${startDate.year}-${startDate.month}-${startDate.day}`;
     const endDateString = endDate.custom ?? `${endDate.year}-${endDate.month}-${endDate.day}`;
 
-    const renderedLinks = links?.length > 0 && <ProjectLinks links={links} />;
-    const renderedTechnologies = technologies?.length > 0 && <ProjectTechnologies technologies={technologies} />;
+    const renderedLinks = links?.length > 0 && <Links links={links} />;
+    const renderedTechnologies = technologies?.length > 0 && <Technologies technologies={technologies} />;
 
     return (
         <article aria-label={`Project: ${title}`} className="bg-white bg-opacity-30 flex justify-center flex-wrap gap-4 overflow-hidden shadow-md rounded-md even:flex-row-reverse sm:justify-start sm:flex-nowrap">
-            {icon && <ProjectIcon icon={icon} iconAlt={iconAlt} lazy={lazy} />}
+            {icon && <Icon icon={icon} iconAlt={iconAlt} lazy={lazy} />}
             <div className="flex flex-col justify-between w-full transition-all duration-500 p-2 gap-6 sm:gap-4 sm:py-2 sm:p-1 md:p-2 lg:p-4">
                 <div className="flex flex-col gap-2 sm:gap-4">
-                    <ProjectHeading
+                    <Heading
                         startDate={startDateString}
                         endDate={endDateString}
                         title={title}
