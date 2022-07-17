916b0d239e0165680ebdc86c20798da55d54a617
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-07-17 16:00:41 +0200

    Create useMediaQuery.jsx

---
+++ src/hooks/useMediaQuery.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+const getMatches = (query) => {
+    if (typeof window !== 'undefined') {
+        return window.matchMedia(query).matches;
+    }
+
+    return false;
+};
+
+function useMediaQuery(query) {
+    const [matches, setMatches] = useState(getMatches(query));
+
+    useEffect(() => {
+        const matchMedia = window.matchMedia(query);
+        const handleChange = () => setMatches(getMatches(query));
+
+        handleChange();
+
+        matchMedia.addEventListener('change', handleChange);
+        return () => {
+            matchMedia.removeEventListener('change', handleChange);
+        };
+    }, [query]);
+
+    return matches;
+}
+
+export default useMediaQuery;
