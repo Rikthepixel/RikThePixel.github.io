acfcb1a6b19037efd0b1dc093be456c360776653
RikThePixel <63408969+Rikthepixel@users.noreply.github.com>
2022-07-17 18:11:09 +0200

    Add more efficient font

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,18 @@
-import "./index.scss";
 import React from "react";
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+
+import "@fontsource/roboto-slab/100.css";
+import "@fontsource/roboto-slab/200.css";
+import "@fontsource/roboto-slab/300.css";
+import "@fontsource/roboto-slab/400.css";
+import "@fontsource/roboto-slab/500.css";
+import "@fontsource/roboto-slab/600.css";
+import "@fontsource/roboto-slab/700.css";
+import "@fontsource/roboto-slab/800.css";
+import "@fontsource/roboto-slab/900.css";
+
+import "./index.scss";
 import Routes from "./routes";
 
 createRoot(
