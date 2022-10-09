b9b3c2130b242d044a05bf2ad552795c219c9b92
RikDePixel <Rikdenbreejen@hotmail.nl>
2022-10-09 13:37:49 +0200

    Move files

---
+++ next.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    distDir: "node_modules/.next",
+    reactStrictMode: true,
+    poweredByHeader: false,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ['@svgr/webpack'],
+        });
+
+        return config;
+    },
+};
+
+module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import React from 'react';
+import { AppProps } from 'next/app';
 
 import "@fontsource/roboto-slab/100.css";
 import "@fontsource/roboto-slab/200.css";
@@ -11,16 +10,13 @@ import "@fontsource/roboto-slab/600.css";
 import "@fontsource/roboto-slab/700.css";
 import "@fontsource/roboto-slab/800.css";
 import "@fontsource/roboto-slab/900.css";
+import "../styles/index.scss";
 
-import "./index.scss";
-import Routes from "./routes";
 
-createRoot(
-    document.getElementById('react-root')
-).render(
-    <React.StrictMode>
-        <HashRouter>
-            <Routes />
-        </HashRouter>
-    </React.StrictMode>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+    return (
+        <Component {...pageProps} />
+    );
+};
+
+export default App;
