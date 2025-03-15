47e32e9e913a1a06c57d1bc6d5f29788700abb21
RikThePixel <rikdenbreejen@tutanota.com>
2025-03-15 13:12:30 +0100

    Fix experiences

--- res/state/experiences.ts
+++ res/state/experiences.ts
@@ -5,43 +5,57 @@ import SquadraMlcLogo from "res/images/logos/squadra-mlc.webp";
 import FontysLogo from "res/images/logos/fontys.webp";
 
 export default [
-    {
-        title: "Software engineer",
-        logo: TinyTronicsLogo,
-        position: "Part-time",
-        place: {
-            name: "TinyTronics B.V.",
-            link: "https://www.tinytronics.nl/shop/en/about-us/"
-        },
-        timespan: {
-            start: "Jun. 2022",
-            end: "Present"
-        }
+  {
+    title: "Software engineer",
+    logo: TinyTronicsLogo,
+    position: "Fulltime",
+    place: {
+      name: "TinyTronics B.V.",
+      link: "https://www.tinytronics.nl/shop/en/about-us/",
     },
-    {
-        title: "Intern Software engineer",
-        logo: SquadraMlcLogo,
-        position: "Fulltime Intern",
-        place: {
-            name: "Squadra Machine Learning Company",
-            link: "https://machine-learning-company.nl/"
-        },
-        timespan: {
-            start: "Aug. 2022",
-            end: "Jan. 2023"
-        }
+    timespan: {
+      start: "Sept. 2024",
+      end: "Present",
     },
-    {
-        title: "Software engineering Student",
-        logo: FontysLogo,
-        position: "Student",
-        place: {
-            name: "Fontys University of Applied Sciences",
-            link: "https://fontys.edu/"
-        },
-        timespan: {
-            start: "2020",
-            end: "Present"
-        }
-    }
-]satisfies IExperience[];
+  },
+  {
+    title: "Software engineer",
+    logo: TinyTronicsLogo,
+    position: "Parttime",
+    place: {
+      name: "TinyTronics B.V.",
+      link: "https://www.tinytronics.nl/shop/en/about-us/",
+    },
+    timespan: {
+      start: "Jun. 2022",
+      end: "Sept. 2024",
+    },
+  },
+  {
+    title: "Intern Software engineer",
+    logo: SquadraMlcLogo,
+    position: "Fulltime Intern",
+    place: {
+      name: "Squadra Machine Learning Company",
+      link: "https://machine-learning-company.nl/",
+    },
+    timespan: {
+      start: "Aug. 2022",
+      end: "Jan. 2023",
+    },
+  },
+  {
+    title: "Software engineering Student",
+    logo: FontysLogo,
+    position: "Student",
+    place: {
+      name: "Fontys University of Applied Sciences",
+      link: "https://fontys.edu/",
+    },
+    timespan: {
+      start: "2020",
+      end: "Present",
+    },
+  },
+] satisfies IExperience[];
+
